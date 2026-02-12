ed14436541f1bafa332599ab307895c63762cd4a
feederbox826 <feederbox826@users.noreply.github.com>
2026-02-12 16:52:07 -0500

    cd to correct directory in cron

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,5 @@ if [ -z "$DISCOURSE_URL_BASE" ] ; then
   echo "DISCOURSE_URL_BASE environment variable must be set. Exiting."
   exit 1
 fi
-/usr/bin/python /app/main.py
+/usr/bin/python3 /app/main.py
 crond -f
